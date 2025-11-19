79ef73850046f76772f55c0c22c844ab8fcf69eb
wiib <s.caezsiguenza@gmail.com>
2025-11-19 12:13:44 -0500

    Add GNOME Polkit agent to autostart

--- mango/autostart.sh
+++ mango/autostart.sh
@@ -7,3 +7,5 @@ dms run >/dev/null 2>&1 &
 wl-clip-persist --clipboard regular --reconnect-tries 0 >/dev/null 2>&1 &
 
 wl-paste --watch cliphist store >/dev/null 2>&1 &
+
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &

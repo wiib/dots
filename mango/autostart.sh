c14a0a4573c613f990d8cf21b32216ab5a74b266
wiib <s.caezsiguenza@gmail.com>
2025-11-18 22:58:16 -0500

    Basic MangoWC config based on dev's dotfiles

---
+++ mango/autostart.sh
@@ -0,0 +1,9 @@
+set +e
+
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots >/dev/null 2>&1
+
+dms run >/dev/null 2>&1 &
+
+wl-clip-persist --clipboard regular --reconnect-tries 0 >/dev/null 2>&1 &
+
+wl-paste --watch cliphist store >/dev/null 2>&1 &

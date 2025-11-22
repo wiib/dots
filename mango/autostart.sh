ef530a69eb7b8c3f9ea49be6b1880a7a9e19d48f
wiib <s.caezsiguenza@gmail.com>
2025-11-22 13:09:01 -0500

    Add Inkscape fix and remove inline comments from spawn commands

--- mango/autostart.sh
+++ mango/autostart.sh
@@ -4,7 +4,7 @@ dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
 
 dms run >/dev/null 2>&1 &
 
-wl-clip-persist --clipboard regular --reconnect-tries 0 >/dev/null 2>&1 &
+wl-clip-persist --clipboard regular --all-mime-type-regex '(?i)^(?!image/x-inkscape-svg).+' >/dev/null 2>&1 &
 
 wl-paste --watch cliphist store >/dev/null 2>&1 &
 
